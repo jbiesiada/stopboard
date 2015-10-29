7af16373eb1b85b1d6cf88804747fca0b94f4531
jbiesiada <jan.biesiada@polcode.net>
2015-10-29 08:50:46 +0100

    search box

--- resources/views/index.php
+++ resources/views/index.php
@@ -22,17 +22,17 @@
 				<p ng-repeat="dep in cityList.deps">{{dep.lineName}} | {{dep.lineEnd}} | {{dep.when}} ({{ dep.hour}}:{{dep.minute}})</p>
 			</div>
 		</div>
+		<div class="col-xs-8" ng-controller="importController as import" ng-show='false'>
+			<h3>{{import.line.name}}</h3>
+			<div class="progress">
+		  		<div class="progress-bar" role="progressbar" aria-valuenow="{{import.percent}}" aria-valuemin="0" aria-valuemax="100" style="width: {{import.percent}}%;">
+		    		{{import.percent | number:2 }}%
+		  		</div>
+			</div>
+			<button ng-show="import.ready" ng-click="import.importStart()">Start</button>
+		</div>
 	</div>
 </div>
 
-<div ng-controller="importController as import" ng-show='false'>
-	<h3>{{import.line.name}}</h3>
-	<div class="progress">
-  		<div class="progress-bar" role="progressbar" aria-valuenow="{{import.percent}}" aria-valuemin="0" aria-valuemax="100" style="width: {{import.percent}}%;">
-    		{{import.percent | number:2 }}%
-  		</div>
-	</div>
-	<button ng-show="import.ready" ng-click="import.importStart()">Start</button>
-</div>
 </body>
 </html>
